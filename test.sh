4dadd59bc5638a9d0c6b84b8eb24c595da9300c5
Jhonatan Paul Urbina Tolentino <jhonatanurbinat@gmail.com>
2020-04-03 18:48:38 -0500

    fix -readme

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+SERVERPROD="localhost"
+
+
+echo "Hola soy $SERVERPROD"
+
 curl --request POST 'http://localhost:8010/oauth/token' \
           --header 'Content-Type: application/x-www-form-urlencoded' \
             --user 'webAngular:123456'  --data-urlencode 'grant_type=password' --data-urlencode 'username=admin' --data-urlencode 'password=12345'
