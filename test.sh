0204162a99bead7760f9eab0061a251b034efe74
Jhonatan Paul Urbina Tolentino <jhonatanurbinat@gmail.com>
2020-04-03 18:16:07 -0500

    add bash

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
-- Ejecutar Shell Compile.sh
-- Ejecutar Comando 
+#!/bin/bash
 
-docker build -t servicio1:0.1 .
 curl --request POST 'http://localhost:8010/oauth/token' \
           --header 'Content-Type: application/x-www-form-urlencoded' \
             --user 'webAngular:123456'  --data-urlencode 'grant_type=password' --data-urlencode 'username=admin' --data-urlencode 'password=12345'
